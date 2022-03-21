ca2ecdadf901ea7c73518e44d0d81024378a6ce7
johnnytest4real <jmaeatmon@icloud.com>
2022-03-20 18:57:50 -0700

    config/feat: add poppins to site & tw config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
