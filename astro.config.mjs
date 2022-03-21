22fc34fc7025812aec22edda4a94f78b64efbab0
johnnytest4real <jmaeatmon@icloud.com>
2022-03-20 17:21:10 -0700

    config: astro settings for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
+  renderers: [],
+  buildOptions: {
+    site: 'https://tacomaporchfest.org',
+    sitemap: true,
+  },
 });
