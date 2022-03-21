ed73c4b11071e2ac9f6f03f6da400e0b07b417b4
johnnytest4real <jmaeatmon@icloud.com>
2022-03-20 17:26:06 -0700

    config: enable tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   renderers: [],
+  integrations: [tailwind()],
   buildOptions: {
     site: 'https://tacomaporchfest.org',
     sitemap: true,
