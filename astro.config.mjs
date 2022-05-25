cc835190cd133e6fd418ff9268f0ed239f9fdf5a
johnnytest4real <jmaeatmon@icloud.com>
2022-05-25 15:42:04 -0700

    config: enable sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
 	experimental: {
 		integrations: true,
 	},
-	integrations: [compress({ html: false }), tailwind()],
+	integrations: [
+		compress({
+			html: false,
+		}),
+		tailwind(),
+		sitemap(),
+	],
 	site: 'https://www.tacomaporchfest.org',
 	vite: {
 		ssr: {
