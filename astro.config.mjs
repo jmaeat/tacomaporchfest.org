e6ada2b26dbbf8f991c9033f5c7e13cb7e9d7426
johnnytest4real <jmaeatmon@icloud.com>
2022-06-19 10:11:09 -0700

    config: enable critters/robots-txt integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
-import tailwind from '@astrojs/tailwind';
+import critters from 'astro-critters';
+import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,9 +13,12 @@ export default defineConfig({
 	integrations: [
 		compress({
 			html: false,
+			svg: false,
 		}),
-		tailwind(),
+		critters(),
+		robotsTxt(),
 		sitemap(),
+		tailwind(),
 	],
 	site: 'https://www.tacomaporchfest.org',
 	vite: {
